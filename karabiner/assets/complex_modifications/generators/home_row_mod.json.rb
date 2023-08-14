9095cabc116383488a37332d3d9378eedc7029eb
Martin Bernstorff <martinbernstorff@gmail.com>
2023-08-14 09:52:14 +0200

    misc.

--- karabiner/assets/complex_modifications/generators/home_row_mod.json.rb
+++ karabiner/assets/complex_modifications/generators/home_row_mod.json.rb
@@ -11,8 +11,8 @@ def main
         "description" => "Shift, Control, Option, Command ⟶  Home Row",
         "manipulators" => [
           # Left Hand
-          generate_hold_modifier_only("a", "left_shift"),
-          generate_hold_modifier_only("s", "left_control"),
+          # generate_hold_modifier_only("a", "left_shift"),
+          # generate_hold_modifier_only("s", "left_control"),
           generate_hold_modifier_only("d", "left_option"),
           generate_hold_modifier_only("f", "left_command"),
           # Right Hand
@@ -23,9 +23,7 @@ def main
           # generate_hold_modifier_only("quote", "right_control"),
         ],
       },
-      {
-        "description" => "Disable left modifier cluster",
-        "manipulators" => [
+      { "description" => "Disable left modifier cluster", "manipulators" => [
           generate_disable_key("left_shift"),
           generate_disable_key("left_control"),
           generate_disable_key("left_option"),
@@ -53,7 +51,7 @@ def generate_hold_modifier_and_if_alone(input, hold_modifier, alone_key_code)
   {
     # "parameters" => basic_parameters(),
     "parameters" => {
-      'basic.to_if_held_down_threshold_milliseconds' => 80, # Modifier if > X
+      'basic.to_if_held_down_threshold_milliseconds' => 100, # Modifier if > X
     },
     "description" => "Hold #{input.split(/ |\_/).map(&:capitalize).join(" ")} ⟶ #{hold_modifier.split(/ |\_/).map(&:capitalize).join(" ")}",
     "type" => "basic",
